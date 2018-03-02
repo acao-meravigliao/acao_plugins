c9273acf6de920e98cf23bf35e9537e8cbcc2a54
Daniele Orlandi <daniele@orlandi.com>
2018-03-02 02:12:23 +0100

    Removed references to UsesGuid

--- trk_models/app/models/ygg/tracking/contest.rb
+++ trk_models/app/models/ygg/tracking/contest.rb
@@ -13,20 +13,14 @@ module Tracking
 class Contest < ActiveRecord::Base
   self.table_name = 'trk_contests'
 
-  include Ygg::Core::UsesGuid
-  uses_guid
-
   has_many :days,
-           :class_name => 'Ygg::Tracking::Contest::Day'
+           class_name: 'Ygg::Tracking::Contest::Day'
 
   class Day < ActiveRecord::Base
     self.table_name = 'trk_contest_days'
 
-    include Ygg::Core::UsesGuid
-    uses_guid
-
     belongs_to :contest,
-               :class_name => 'Ygg::Tracking::Contest'
+               class_name: 'Ygg::Tracking::Contest'
   end
 end
 
