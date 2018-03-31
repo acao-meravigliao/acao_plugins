75b84600745341ba0e6c94155d5808a43cbde9d1
Daniele Orlandi <daniele@orlandi.com>
2018-03-31 14:34:02 +0200

    Added trailer model

---
+++ acao_models/app/models/ygg/acao/trailer.rb
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2017-2018, Daniele Orlandi
+#
+# Author:: Daniele Orlandi <daniele@orlandi.com>
+#
+# License:: You can redistribute it and/or modify it under the terms of the LICENSE file.
+#
+
+
+module Ygg
+module Acao
+
+class Trailer < Ygg::PublicModel
+  self.table_name = 'acao_trailers'
+
+  belongs_to :person,
+             class_name: '::Ygg::Core::Person'
+
+  belongs_to :payment,
+             class_name: 'Ygg::Acao::Payment',
+             optional: true
+
+  include Ygg::Core::Loggable
+  define_default_log_controller(self)
+
+#  has_acl
+#
+#  include Ygg::Core::Notifiable
+#
+#  def set_default_acl
+#    transaction do
+#      acl_entries.where(owner: self).destroy_all
+#      acl_entries << AclEntry.new(owner: self, person: person, capability: 'owner')
+#    end
+#  end
+
+end
+
+end
+end
